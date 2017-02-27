4e807ec28216d1926ec028b3deb1f8d9f1419a72
Eric Eakin <eseakin@gmail.com>
2017-02-26 23:36:57 -0800

    Added login form as modular component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+// import { TextInput } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '' };
+
+
   render() {
     return (
       <Card>
-        <CardSection/>
-        <CardSection/>
+        <CardSection>
+          <Input 
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            placeholder="user@gmail.com"
+          />
+        </CardSection>
+        
+        <CardSection>
+          <Input />
+        </CardSection>
+        
         <CardSection>
           <Button>
             Login
